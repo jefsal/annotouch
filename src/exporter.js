a1db3f47aa101ca161b36f05570d9653b82d0ffe
Jeffrey Salinas <159089127+jefsal@users.noreply.github.com>
2026-07-23 15:24:02 -0700

    Revert "add keyboard text annotations"

--- src/exporter.js
+++ src/exporter.js
@@ -1,91 +1,42 @@
-import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
+import { PDFDocument, rgb } from "pdf-lib";
 
 export async function exportAnnotatedPdf({
   originalBytes,
-  annotationsByPage,
+  strokesByPage,
   pageViewports,
   scale,
   sourceFileName,
 }) {
   const pdfDoc = await PDFDocument.load(originalBytes);
   const pages = pdfDoc.getPages();
-  let textFont = null;
 
-  for (const [pageNumber, annotations] of annotationsByPage) {
+  for (const [pageNumber, strokes] of strokesByPage) {
     const page = pages[pageNumber - 1];
     const viewport = pageViewports.get(pageNumber);
 
     if (!page || !viewport) continue;
 
-    for (const annotation of annotations) {
-      const color = hexToRgb(annotation.color);
+    for (const stroke of strokes) {
+      const color = hexToRgb(stroke.color);
 
-      if (annotation.type === "text") {
-        textFont ??= await pdfDoc.embedFont(StandardFonts.Helvetica);
-        drawTextAnnotation({
-          annotation,
-          color,
-          font: textFont,
-          page,
-          scale,
-          viewport,
+      for (let index = 1; index < stroke.points.length; index += 1) {
+        const start = stroke.points[index - 1];
+        const end = stroke.points[index];
+        const [startX, startY] = viewport.convertToPdfPoint(start.x, start.y);
+        const [endX, endY] = viewport.convertToPdfPoint(end.x, end.y);
+
+        page.drawLine({
+          start: { x: startX, y: startY },
+          end: { x: endX, y: endY },
+          thickness: stroke.width / scale,
+          color: rgb(color.r, color.g, color.b),
         });
-        continue;
       }
-
-      drawStrokeAnnotation({
-        annotation,
-        color,
-        page,
-        scale,
-        viewport,
-      });
     }
   }
 
   const bytes = await pdfDoc.save();
   downloadBytes(bytes, getExportName(sourceFileName));
-}
-
-function drawStrokeAnnotation({ annotation, color, page, scale, viewport }) {
-  for (let index = 1; index < annotation.points.length; index += 1) {
-    const start = annotation.points[index - 1];
-    const end = annotation.points[index];
-    const [startX, startY] = viewport.convertToPdfPoint(start.x, start.y);
-    const [endX, endY] = viewport.convertToPdfPoint(end.x, end.y);
-
-    page.drawLine({
-      start: { x: startX, y: startY },
-      end: { x: endX, y: endY },
-      thickness: annotation.width / scale,
-      color: rgb(color.r, color.g, color.b),
-    });
-  }
-}
-
-function drawTextAnnotation({
-  annotation,
-  color,
-  font,
-  page,
-  scale,
-  viewport,
-}) {
-  const fontSize = annotation.fontSize / scale;
-  const lineHeight = annotation.lineHeight / scale;
-  const [x, topY] = viewport.convertToPdfPoint(annotation.x, annotation.y);
-
-  annotation.text.split("\n").forEach((line, index) => {
-    if (line.length === 0) return;
-
-    page.drawText(line, {
-      x,
-      y: topY - fontSize - index * lineHeight,
-      size: fontSize,
-      font,
-      color: rgb(color.r, color.g, color.b),
-    });
-  });
 }
 
 function hexToRgb(hex) {
